6289ec7aaf353e88fdba69a3af9aa9f038d3efbc
Glauber Lucas <brittoglauber@gmail.com>
2022-06-18 17:55:26 -0300

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+
+function About() {
+  return (
+    <div name="about" className="2-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Sobre</p>
+        </div>
+        <p className="text-xl mt-20">Desenvolvedor web, Iniciei no desenvolvimento em 2021, sou amante de novas tecnologias do mercado, principalmente voltadas para backend. No momento trabalho com ReactJS, NodeJS, Javascript e Typescript e outras tecnologias para criacão de aplicações web, Por hora também estudo segurança da informação voltada para aplicações web. Me considero um pesquisador de tecnologia, portanto estou à disposição para aprender linguagens novas e a me adequar a qualquer mudança, pronto para superar qualquer desafio !</p> 
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Contact.jsx
@@ -0,0 +1,51 @@
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Cotato
+          </p>
+          <p className="py-6">Preencha o formulário a seguir para entra em contato</p>
+        </div>
+
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/315ee46e-be5a-4af4-81b3-f1e312e756f2"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Seu nome"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Seu email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Deixe sua mensagem "
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Vamos conversar 
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
